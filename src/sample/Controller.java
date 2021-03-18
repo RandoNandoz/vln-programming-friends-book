e1c8089cfac5ea6c872490d8f67ec5fa629699be
Randy Zhu <randyzhu@protonmail.com>
2021-03-18 15:40:02 -0700

    Finished the edit logic. This was kinda hard.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -26,35 +26,165 @@ public class Controller {
 
   // When the "Create Friend" button is clicked.
   public void createFriendAction(MouseEvent mouseEvent) {
+
+    // Call getUserInfo, and add that to the list.
+    Friend friend = getUserInfo();
+    // Otherwise, do not create the object, as there is an invalid object.
+    if (friend != null) {
+      friendList.getItems().add(friend);
+    }
+  }
+
+  // When the "Delete Friend" button is clicked.
+  public void deleteFriendAction(MouseEvent mouseEvent) {
+    // We get the user's selected friend.
+    Friend selectedFriend = friendList.getSelectionModel().getSelectedItem();
+    // If there is a valid selection, we alert the users to their requested deletion,
+    if (selectedFriend != null) {
+      Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+      alert.setTitle("Confirm Deletion");
+      alert.setHeaderText("You are about to remove a friend.");
+      alert.setContentText("Are you sure you would like to remove " + selectedFriend.getName() + "?");
+      Optional<ButtonType> result = alert.showAndWait();
+      if (result.isPresent()) {
+        if (result.get() == ButtonType.OK) {
+          friendList.getItems().remove(selectedFriend);
+        }
+      }
+    } else {
+      popInvalidOptionAlert("You did not select a friend to delete, please try again.");
+    }
+  }
+
+  // When the "View Friend Details" button is clicked.
+  public void viewFriendDetailsAction(MouseEvent mouseEvent) {
+    // We get the user's selected field.
+    Friend selectedFriend = friendList.getSelectionModel().getSelectedItem();
+    // If there actually is a selection, then we query all the values, and display it on all the fields.
+    if (selectedFriend != null) {
+      nameField.setText(selectedFriend.getName());
+      ageField.setText(String.valueOf(selectedFriend.getAge()));
+      genderField.setText(selectedFriend.getGender());
+      birthdateCalendarPicker.setValue(selectedFriend.getBirthDate());
+      birthdateCalendarPicker.setDisable(true);
+      closenessDropdown.setText(selectedFriend.getCloseness());
+      notesField.setText(selectedFriend.getNotes());
+    }
+  }
+
+  // When the acquaintance bit on the closeness dropdown is clicked.
+  public void acquaintanceItemClicked(ActionEvent actionEvent) {
+    closenessDropdown.setText(acquaintanceItem.getText());
+  }
+
+  // When the friend bit on the closeness dropdown is clicked.
+  public void friendItemClicked(ActionEvent actionEvent) {
+    closenessDropdown.setText(friendItem.getText());
+  }
+
+  // When the best friend bit on the closeness dropdown is clicked.
+  public void bestFriendItemClicked(ActionEvent actionEvent) {
+    closenessDropdown.setText(bestFriendItem.getText());
+  }
+
+  // Commit to the edits made when "view friend details" made.
+  public void editFriendAction(MouseEvent mouseEvent) {
+    // Get the selected friend's index.
+    int selectedFriendIndex = friendList.getSelectionModel().getSelectedIndex();
+    // Now, we query the fields again for any changes.
+    Friend friend = getUserInfo();
+    // If it's valid (which it should be), set that index to the new friend we just queried. We basically swap.
+    if (friend != null) {
+      friendList.getItems().set(selectedFriendIndex, friend);
+    }
+    resetFields();
+  }
+
+  // Function to pop an alert.
+  private void popInvalidOptionAlert(String msg) {
+    // Instantiate the alert object, then set the contents of the alert, and then show it.
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    alert.setHeaderText("Invalid Option");
+    alert.setContentText(msg);
+    alert.show();
+  }
+
+  // Reset and enable all the fields.
+  private void resetFields() {
+    // Clear and enable all the fields.
+    nameField.setText("");
+    nameField.setDisable(false);
+    ageField.setText("");
+    ageField.setDisable(false);
+    genderField.setText("");
+    genderField.setDisable(false);
+    birthdateCalendarPicker.setValue(null);
+    birthdateCalendarPicker.setDisable(false);
+    closenessDropdown.setText("Closeness");
+    closenessDropdown.setDisable(false);
+    notesField.setText("");
+    notesField.setDisable(false);
+  }
+
+  // Get the gender from the params and the hashsets provided.
+  private String getGender(TextField textField, HashSet<String> wordsForFemale, HashSet<String> wordsForMale) {
+
+    /*
+    * One may ask: Why HashSet?
+    * Now, isn't that a great question. See, with an ArrayList, the ArrayList.contains(Object) method is in O(n), while HashSet.contains(Object) is O(1).
+    * The lookup speed is constant, making it better. However, it doesn't really matter for like 4 items. Oh well.
+    * */
+
+    // This function gets the gender from the TextField which is provided to it.
+    String returnVal;
+    // If the TextField has text which is contained in the wordsForFemale hashset, then assign female.
+    if (wordsForFemale.contains(textField.getText().toLowerCase())) {
+      returnVal = Friend.GENDER_FEMALE;
+      // If the TextField has text which is in the wordsForMale hashset, then assign male.
+    } else if (wordsForMale.contains(textField.getText().toLowerCase())) {
+      returnVal = Friend.GENDER_MALE;
+      // If it's empty, we throw an exception which we handle.
+    } else if (textField.getText().equals("")) {
+      throw new RuntimeException();
+      // Alas, if it's neither female, male, nor empty, we assume that they do not identify with either male or female,
+      // and just capitalize the first letter.
+    } else {
+      returnVal = textField.getText().substring(0, 1).toUpperCase() + textField.getText().substring(1).toLowerCase();
+    }
+    return returnVal;
+  }
+
+  // Get the closeness from the dropdown menu.
+  private int getCloseness(MenuButton menuButton) {
+    // Switch statement that we return, maps the text selected from the menu button to the constant values in the friend class.
+    // If the user doesn't choose, throw an exception which we can handle later.
+    return switch (menuButton.getText()) {
+      case "Acquaintance" -> Friend.CLOSENESS_ACQUAINTANCE;
+      case "Friend" -> Friend.CLOSENESS_FRIEND;
+      case "Best Friend" -> Friend.CLOSENESS_BEST_FRIEND;
+      case "Closeness" -> throw new RuntimeException();
+      default -> Friend.CLOSENESS_INVALID;
+    };
+  }
+
+  // Big boi method which kinda just gets all the data.
+  private Friend getUserInfo() {
     // Set the name variable with the text in the name field.
     String name = nameField.getText();
 
     // Then we set the age, if the age is invalid, tell the user.
     int age = -1;
     try {
+      // Call parseInt() on the ageField, to get the age. If it's invalid, we catch the error, and tell the user.
       age = Integer.parseInt(ageField.getText());
     } catch (NumberFormatException e) {
       popInvalidOptionAlert("Invalid entry for field age, please try again.");
     }
 
-    /*
-     * My way of getting the gender from the TextField. Using the hashsets containing the words for female and male, I check whether the text of the TextField
-     * is in the hashset using HashSet.contains(<object>), then assigning it to the gender variable. This is superior to the ArrayList.contains(<object>), as
-     * the time complexity of HashSet.contains() is average O(1), while for the ArrayList, it's O(n).
-     *
-     * If the field is empty, alert the user.
-     *  */
+    // Query for gender.
     String gender = null;
     try {
-      if (wordsForFemale.contains(genderField.getText().toLowerCase())) {
-        gender = Friend.GENDER_FEMALE;
-      } else if (wordsForMale.contains(genderField.getText().toLowerCase())) {
-        gender = Friend.GENDER_MALE;
-      } else if (genderField.getText().equals("")) {
-        throw new RuntimeException();
-      } else {
-        gender = genderField.getText().substring(0, 1).toUpperCase() + genderField.getText().substring(1).toLowerCase();
-      }
+      gender = getGender(genderField, wordsForFemale, wordsForMale);
     } catch (RuntimeException e) {
       popInvalidOptionAlert("Gender field is empty, please input valid value.");
     }
@@ -69,13 +199,7 @@ public class Controller {
     // Closeness switch. We turn the text from the dropdown into numerical representations. In this case, if the doesn't choose, then it'll alert the user to an error.
     int closeness = Friend.CLOSENESS_INVALID;
     try {
-      closeness = switch (closenessDropdown.getText()) {
-        case "Acquaintance" -> Friend.CLOSENESS_ACQUAINTANCE;
-        case "Friend" -> Friend.CLOSENESS_FRIEND;
-        case "Best Friend" -> Friend.CLOSENESS_BEST_FRIEND;
-        case "Closeness" -> throw new RuntimeException();
-        default -> Friend.CLOSENESS_INVALID;
-      };
+      closeness = getCloseness(closenessDropdown);
     } catch (RuntimeException e) {
       popInvalidOptionAlert("You did not select a proper value for closeness, please try again.");
     }
@@ -83,82 +207,12 @@ public class Controller {
     // Set notes to the text in the notes field.
     String notes = notesField.getText();
 
-    // Now, we start with friend at null, and we check for valid values for all the variables, extracted from the fields. If they are all valid, we create a new
-    // friend object, add it to the list, and then we clear all the fields.
+    // Check if there are any invalid values, if so, return null, otherwise,
     Friend friend = null;
     if (name != null && age != -1 && gender != null && birthDate != null && closeness != Friend.CLOSENESS_INVALID) {
       friend = new Friend(name, age, gender, birthDate, closeness, notes);
-      nameField.setText("");
-      ageField.setText("");
-      genderField.setText("");
-      birthdateCalendarPicker.setValue(null);
-      closenessDropdown.setText("Closeness");
-      notesField.setText("");
+      resetFields();
     }
-    // Otherwise, do not create the object, as there is an invalid object.
-    if (friend != null) {
-      friendList.getItems().add(friend);
-    }
-  }
-
-  // When the "Delete Friend" button is clicked.
-
-  public void deleteFriendAction(MouseEvent mouseEvent) {
-    Friend selectedFriend = friendList.getSelectionModel().getSelectedItem();
-    if (selectedFriend != null) {
-      Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-      alert.setTitle("Confirm Deletion");
-      alert.setHeaderText("You are about to remove a friend.");
-      alert.setContentText("Are you sure you would like to remove " + selectedFriend + "?");
-      Optional<ButtonType> result = alert.showAndWait();
-      if (result.isPresent()) {
-        if (result.get() == ButtonType.OK) {
-          friendList.getItems().remove(selectedFriend);
-        }
-      }
-    } else {
-      popInvalidOptionAlert("You did not select a friend to delete, please try again.");
-    }
-  }
-  // When the "View Friend Details" button is clicked.
-
-  public void viewFriendDetailsAction(MouseEvent mouseEvent) {
-    Friend selectedFriend = friendList.getSelectionModel().getSelectedItem();
-    if (selectedFriend != null) {
-      nameField.setText(selectedFriend.getName());
-      ageField.setText(String.valueOf(selectedFriend.getAge()));
-      genderField.setText(selectedFriend.getGender());
-      birthdateCalendarPicker.setValue(selectedFriend.getBirthDate());
-      birthdateCalendarPicker.setDisable(true);
-      closenessDropdown.setText(selectedFriend.getCloseness());
-      notesField.setText(selectedFriend.getNotes());
-    }
-  }
-  // When the acquaintance bit on the closeness dropdown is clicked.
-
-  public void acquaintanceItemClicked(ActionEvent actionEvent) {
-    closenessDropdown.setText(acquaintanceItem.getText());
-  }
-  // When the friend bit on the closeness dropdown is clicked.
-
-  public void friendItemClicked(ActionEvent actionEvent) {
-    closenessDropdown.setText(friendItem.getText());
-  }
-
-  public void bestFriendItemClicked(ActionEvent actionEvent) {
-    closenessDropdown.setText(bestFriendItem.getText());
-  }
-
-  // Function to pop an alert.
-  public void popInvalidOptionAlert(String msg) {
-    // Instantiate the alert object, then set the contents of the alert, and then show it.
-    Alert alert = new Alert(Alert.AlertType.ERROR);
-    alert.setHeaderText("Invalid Option");
-    alert.setContentText(msg);
-    alert.show();
-  }
-
-  public void editFriendAction(MouseEvent mouseEvent) {
-    Friend selectedFriend = friendList.getSelectionModel().getSelectedItem();
+    return friend;
   }
 }
--- src/sample/Friend.java
+++ src/sample/Friend.java
@@ -3,24 +3,22 @@ package sample;
 import java.time.LocalDate;
 
 public class Friend {
-  // Attributes for the friend.
-  private String name;
-  private int age;
-  private String gender;
-  private final LocalDate birthDate;
-  private int closeness;
-  private String notes;
-
   // Potential options for gender.
   public static final String GENDER_FEMALE = "Female";
   public static final String GENDER_MALE = "Male";
-
   // Potential options for closeness.
   public static final int CLOSENESS_ACQUAINTANCE = 0;
   public static final int CLOSENESS_FRIEND = 1;
   public static final int CLOSENESS_BEST_FRIEND = 2;
   // Invalid case for an invalid value.
   public static final int CLOSENESS_INVALID = -1;
+  private final LocalDate birthDate;
+  // Attributes for the friend.
+  private String name;
+  private int age;
+  private String gender;
+  private int closeness;
+  private String notes;
 
   // Constructor for the Friend class.
   public Friend(String name, int age, String gender, LocalDate birthDate, int closeness, String notes) {
@@ -47,12 +45,24 @@ public class Friend {
     return name;
   }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public int getAge() {
     return age;
   }
 
+  public void setAge(int age) {
+    this.age = age;
+  }
+
   public String getGender() {
     return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
   }
 
   public LocalDate getBirthDate() {
@@ -60,13 +70,18 @@ public class Friend {
   }
 
   public String getCloseness() {
+    // Unlike the switch in the Controller.getCloseness() function, this getCloseness function converts the constants to strings, basically the opposite of it.
     return switch (closeness) {
       case CLOSENESS_ACQUAINTANCE -> "Acquaintance";
-      case CLOSENESS_FRIEND -> "Friends";
+      case CLOSENESS_FRIEND -> "Friend";
       case CLOSENESS_BEST_FRIEND -> "Best Friend";
       case CLOSENESS_INVALID -> "INVALID!!! SOMETHING HAS GONE WRONG!!!";
       default -> throw new IllegalStateException("Unexpected value: " + closeness);
     };
+  }
+
+  public void setCloseness(int closeness) {
+    this.closeness = closeness;
   }
 
   public int getRawClosenessValue() {
@@ -75,22 +90,6 @@ public class Friend {
 
   public String getNotes() {
     return notes;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  public void setGender(String gender) {
-    this.gender = gender;
-  }
-
-  public void setCloseness(int closeness) {
-    this.closeness = closeness;
   }
 
   public void setNotes(String notes) {
